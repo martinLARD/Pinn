8fbb111a4d918f03159359c8d1a95d62e645f332
martinLARD <martinlardy.ml@gmail.com>
2023-11-15 15:40:59 +0100

    ploting code

--- sanstitre2.py
+++ sanstitre2.py
@@ -4,19 +4,20 @@ import matplotlib.pyplot as plt
 
 
 
-df = np.loadtxt('loss.dat').T
+df = np.loadtxt('loss1.dat').T
 plt.title('losses')
-#plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[1],label="loss phy")
-plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[2],label="loss tot")
-plt.xlabel('Epoch (e1)')
+plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[1],label="loss phy")
+plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[0],label="loss data")
+plt.xlabel('Epoch')
 plt.legend()
+plt.yscale("log")
+
 plt.show()
 
-df = np.loadtxt('lbd.dat').T
+df = np.loadtxt('lbd1.dat').T
 plt.title('$\lambda_1$')
 #plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[1],label="loss phy")
 plt.plot(np.linspace(0,len(df),len(df)),df,label="loss data")
 plt.xlabel('Epoch (e3)')
 plt.legend()
-plt.yscale("log")
 plt.show()
