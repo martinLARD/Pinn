99c7f808d50e3b17e9f99133444519857503e320
martinLARD <martinlardy.ml@gmail.com>
2023-11-15 15:45:52 +0100

    ploting code

--- sanstitre2.py
+++ sanstitre2.py
@@ -18,6 +18,6 @@ df = np.loadtxt('lbd1.dat').T
 plt.title('$\lambda_1$')
 #plt.plot(np.linspace(0,len(df[1]),len(df[1])),df[1],label="loss phy")
 plt.plot(np.linspace(0,len(df),len(df)),df,label="loss data")
-plt.xlabel('Epoch (e3)')
+plt.xlabel('Epoch')
 plt.legend()
 plt.show()
